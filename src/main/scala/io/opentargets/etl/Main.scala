641e893fc0084f136ae41be213d43e60b361df79
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-04-21 14:15:47 +0100

    Test

--- src/main/scala/io/opentargets/etl/Main.scala
+++ src/main/scala/io/opentargets/etl/Main.scala
@@ -2,53 +2,27 @@ package io.opentargets.etl
 
 import com.typesafe.scalalogging.LazyLogging
 import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigRenderOptions}
-
-import scala.collection.mutable.HashMap
-import play.api.libs.json.{JsPath, Json, Reads, Writes, Format}
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types._
-import org.apache.spark.sql._
-//import com.typesafe.scalalogging.LazyLogging
+import io.opentargets.etl.backend._
 
 
-object Configuration extends LazyLogging {
-  case class Resident(name: String, age: Int, role: Option[String])
-  implicit val residentReads = Json.reads[Resident]
-	
+object ETL extends LazyLogging {
+  def apply(step: String) = {
+    val otc = Configuration.load
 
+    implicit val spark = SparkSessionWrapper.session
 
-  def load: Config = {
-    logger.info("load configuration from file")
-	print("load from config file,,,")
-    ConfigFactory.load()
+    step match {
+      case "target" =>
+        logger.info("run step target")
+        Target(otc)
+      case _ =>
+        logger.error("Exit with error or ALL by defaul (?) ")
+    }
   }
-
 }
 
 object Main {
 	def main(args: Array[String]): Unit = {
-	    println("Hello from main scala object")
-		val otc = Configuration.load
-		val value = otc.getString("ot.common.inputs.target.path")
-		println(s"My secret value is $value")
-	    print("cinzia")
-		//val common = Configuration.loadCommon(otc)
-		
-		val sparkConf = new SparkConf()
-	      .set("spark.driver.maxResultSize", "0")
-	      .set("spark.debug.maxToStringFields", "2000")
-	      .setAppName("etl-generation")
-	      .setMaster("local[*]")
-
-	    lazy val session: SparkSession =
-	      SparkSession
-	        .builder()
-	        .config(sparkConf)
-	        .getOrCreate
-	    //ETLStep("target")
-	    val ciccia = session.read.format("parquet").load(value)
-		print(ciccia.printSchema)
-	  }
+        ETL("target")
+    }
 }

5e9f48590fa50fa9d09ca527ddc4533ca6f07b97
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-04-28 15:25:34 +0100

    vector options

--- src/main/scala/io/opentargets/etl/Main.scala
+++ src/main/scala/io/opentargets/etl/Main.scala
@@ -1,37 +1,58 @@
 package io.opentargets.etl
 
 import com.typesafe.scalalogging.LazyLogging
-import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigRenderOptions}
+import com.typesafe.config.{
+  Config,
+  ConfigFactory,
+  ConfigObject,
+  ConfigRenderOptions
+}
 import io.opentargets.etl.backend._
 
 object ETL extends LazyLogging {
+  val allSteps: Vector[String] = Vector(
+    "search",
+    "associations",
+    "associationsLLR",
+    "clinicalTrials",
+    "evidenceDrugDirect",
+    "disease",
+    "target",
+    "reactome",
+    "eco",
+    "drug",
+    "cancerBiomarkers",
+    "dailymed",
+    "ddr"
+  )
 
-	def applySingleStep(step:String, otc: Config) = {
-	    step match {
-	      case "disease" =>
-	        logger.info("run step disease")
-	      case "target" =>
-	        logger.info("run step target")
-	      case "reactome" =>
-	        logger.info("run step reactome (rea)")
-	      case "eco" =>
-	        logger.info("run step eco")
-	      case _ =>
-	        logger.error("Exit with error or ALL by defaul (?) ")
-	    }
-	}
-
+  def applySingleStep(step: String, otc: Config) = {
+    step match {
+      case "disease" =>
+        logger.info("run step disease")
+      case "target" =>
+        logger.info("run step target")
+      case "reactome" =>
+        logger.info("run step reactome (rea)")
+      case "eco" =>
+        logger.info("run step eco")
+      case _ =>
+        logger.error("Exit with error or ALL by defaul (?) ")
+    }
+  }
 
   def apply(step: String) = {
     val otc = Configuration.load
-
     implicit val spark = SparkSessionWrapper.session
 
     step match {
       case "all" =>
         logger.info("RUN ALL")
+        for( singleStep <- allSteps ){
+           ETL.applySingleStep(singleStep, otc)
+        }
       case _ =>
-	    ETL.applySingleStep(step, otc)
+        ETL.applySingleStep(step, otc)
     }
   }
 }
